f1d2cfefef47ed9b3914cf4787edbdb05d509b01
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-08-20 10:42:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,71 @@ handler.loadFunctions(`./functions`);
   
 
 
+bot.functionManager.createFunction(
+    {
+      name: "$lockThread",
+      type: "djs",
+      code: async (d) => {
+        const data = d.util.aoiFunc(d);
+        const [threadId] = data.inside.splits;
+  
+        const thread = await d.message.client.channels.fetch(threadId);
+  
+        try {
+          await thread.setLocked(true);
 
+          return {
+            code: d.util.setCode(data),
+          };
+        } catch (error) {
+          console.error(error);
+          return d.aoiError.fnError(d, 'custom', {}, 'Failed to lock thread');
+        }
+      },
+    },
+    {
+      name: "$unlockThread",
+      type: "djs",
+      code: async (d) => {
+        const data = d.util.aoiFunc(d);
+        const [threadId] = data.inside.splits;
+  
+        const thread = await d.message.client.channels.fetch(threadId);
+  
+        try {
+          await thread.setLocked(false);
+
+          return {
+            code: d.util.setCode(data),
+          };
+        } catch (error) {
+          console.error(error);
+          return d.aoiError.fnError(d, 'custom', {}, 'Failed to unlock thread');
+        }
+      },
+    },
+    {
+      name: "$renameThread",
+      type: "djs",
+      code: async (d) => {
+        const data = d.util.aoiFunc(d);
+        const [threadId, newName] = data.inside.splits;
+  
+        try {
+          const thread = await d.message.client.channels.fetch(threadId);
+  
+          await thread.setName(newName);
+
+          return {
+            code: d.util.setCode(data),
+          };
+          
+        } catch (error) {
+          console.error(error);
+          return d.aoiError.fnError(d, 'custom', {}, 'Failed to rename thread');
+        }
+      },
+    });
 bot.awaitedCommand({
   name: "null",
   code: `$setGuildVar[auth;Null]
