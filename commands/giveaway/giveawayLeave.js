92f59081d463a2e367cb7f36b2692ad06552e913
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-08-21 15:13:41 +0700

    Add files via upload

---
+++ commands/giveaway/giveawayLeave.js
@@ -0,0 +1,27 @@
+module.exports ={
+  type: "interaction",
+  prototype: "button",
+  code:`
+
+$editMessage[$get[id];{newEmbed:{title:$getEmbed[$channelID;$get[id];1;title]}{color:Aqua}{footer:$getEmbed[$channelID;$get[id];1;footertext]}
+{description:$splitText[1]**$math[$splitText[2]-1]**$splitText[3]}}{actionRow:{button::2:giveawayJoin.$authorID:false:🎉}};$channelId]
+
+
+$interactionUpdate[Successfully left the giveaway!]
+
+$textSplit[$getEmbed[$channelID;$get[id];1;description];**]
+
+  $writeFile[$get[path];$getObject;utf8]
+
+  $addObjectProperty[a$guildID_$get[id];{"author": "$getObjectProperty[a$guildID_$get[id].author]", "users": "$replaceText[$getObjectProperty[a$guildID_$get[id].users];|$authorID;]","prize":"$getObjectProperty[a$guildID_$get[id].prize]", "winners": "$getObjectProperty[a$guildID_$get[id].winners]", "time": "$getObjectProperty[a$guildID_$get[id].time]", "url": "$getObjectProperty[a$guildID_$get[id].url]"}]
+  $createObject[$nonEscape[$get[data]]]
+
+  $let[data;$readFile[$get[path]]]
+  
+  $let[path;./giveaways.sql]
+ $let[id;$splitText[2]]
+
+ $onlyIf[$splitText[1]==giveawayLeave]
+  $textSplit[$interactionData[customId];.]
+  `
+}
---
+++ commands/giveaway/giveawayStart.js
@@ -0,0 +1,35 @@
+module.exports ={
+  name: "giveaway start",
+  aliases: "gws",
+  category: "giveaway",
+  usage: "{prefix}giveaway start 12h 3 Merchandise",
+  code:`
+
+
+  
+
+$writeFile[$get[path];$getObject;utf8]
+$addObjectProperty[a$guildID_$get[id];{"author": "$authorID", "users": "$clientID", "prize": "$messageSlice[2]", "winners": "$message[2]", "time": "$truncate[$math[($dateStamp+$parseTime[$message[1]])/1000]]", "url": "$guildID/$channelID/$get[id]", "timeoutId": "$get[timeoutId]"}]
+$createObject[$nonEscape[$get[data]]]
+$let[data;$readFile[$get[path]]]
+$let[path;./giveaways.sql]
+ 
+ $let[timeoutId;$setTimeout[giveawayEnd;$message[1];{"channel": "$channelID", "message": "$get[id]", "guild": "$guildID"};true]]
+
+
+$let[id;$sendMessage[{newEmbed:{title:$messageSlice[2]}
+{description:Click 🎉 button to participate!\n\nEnds#COLON# <t#COLON#$truncate[$math[($dateStamp+$parseTime[$message[1]])/1000]]#COLON#R>
+Entries#COLON# **0**
+Winner(s)#COLON# Pending!
+Hosted by#COLON# <@$authorID>}
+{footer:$message[2] winner(s) to be choosen | $parseDate[$math[$dateStamp+$parseTime[$message[1]]];date]}
+{color:Aqua}}{actionRow:{button::2:giveawayJoin.$authorID:false:🎉}};true]]
+
+
+
+$onlyIf[$parseTime[$message[1]]!=-1;Invalid time provided.]
+  
+  
+  `
+}
+// $onlyIf[$parseTime[$message[1]]>=60000;Time cannot be less than 1 minute]
