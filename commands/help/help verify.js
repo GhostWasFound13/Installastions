10b08464a362c521e7d6e40935b528bdbcfd2f9a
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-08-23 18:43:59 +0700

    Create help verify.js

---
+++ commands/help/help verify.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "help verify",
+  descriptions: "to see all verify command",
+  code: `$title[1; ** here all my verify command*]
+  $description[1; 
+  1. {prefix}setVerifyRole {role}
+  2. {prefix}verify 
+  ]
+  $footer[1;requesting by $username]
+  $color[1;RANDOM]
+  
