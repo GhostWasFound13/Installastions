4fd0c719e8b849f67d523f68bae05daf642dfdd0
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-08-31 13:15:03 +0700

    Create antiCrash.js

---
+++ handler/antiCrash.js
@@ -0,0 +1,17 @@
+const logger = require("../logger.js")
+module.exports = (bot) => {
+    process.on('unhandledRejection', (reason, p) => {
+        client.logger.warn(' [antiCrash] :: Unhandled Rejection/Catch');
+        console.log(reason, p);
+    });
+    process.on("uncaughtException", (err, origin) => {
+        client.logger.warn(' [antiCrash] :: Uncaught Exception/Catch');
+        console.log(err, origin);
+    }); process.on('uncaughtExceptionMonitor', (err, origin) => {
+        client.logger.warn(' [antiCrash] :: Uncaught Exception/Catch (MONITOR)');
+        console.log(err, origin);
+    });
+    process.on('multipleResolves', (type, promise, reason) => {
+        client.logger.warn(' [antiCrash] :: Multiple Resolves');
+    });
+}
