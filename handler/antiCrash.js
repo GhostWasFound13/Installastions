5b2eb7913264a4490417cf4392d88a80ebdca84f
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-08-31 13:15:36 +0700

    Update antiCrash.js

--- handler/antiCrash.js
+++ handler/antiCrash.js
@@ -1,4 +1,4 @@
-const logger = require("../logger.js")
+const logger = require("./logger.js")
 module.exports = (bot) => {
     process.on('unhandledRejection', (reason, p) => {
         client.logger.warn(' [antiCrash] :: Unhandled Rejection/Catch');
