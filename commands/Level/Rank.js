ee55875daff53a35d93349ed832f233415f19330
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-09-04 15:02:59 +0700

    Unfinished

--- commands/Level/Rank.js
+++ commands/Level/Rank.js
@@ -1,6 +1,40 @@
 module.exports = [{
-  name: "level",
-  code: `$author[👍| your levels;$userAvatar[$authorID]]
+  name: "rank",
+  code: `const canvacord = require('canvacord');
+    const discord = require('discord.js');
+    
+    const rank = new canvacord.Rank()
+ .setOverlay('#381170', 0, false)  
+.setProgressBarTrack('#2b0c56')
+.setFontSize('5') 
+.setProgressBar('#381170, true')
+
+.setAvatar('$replaceText[$replaceText[$userAvatar[$mentioned[1]];webp;png];gif;png]')
+    .setUsername('$replaceTextWithRegex[$username[$mentioned[1]];[^a-z A-Z 0-9 а-я А-Я\s]+;g;]')
+   
+ 
+.setDiscriminator('$discriminator[$mentioned[1]]')
+    .setStatus('$status[$mentioned[1]]')
+    .setLevel($getUserVar[lvl;$mentioned[1]],'Ур. ')
+
+   
+.setRank($getLeaderboardInfo[lvl;$mentioned[1];user;top], 'Топ')
+  .setCurrentXP($getUserVar[xp;$mentioned[1]])
+    .setRequiredXP($getUserVar[maxxp;$mentioned[1]])
+    .setBackground('IMAGE', '$getUserVar[fon]')
+   
+    
+    rank.build()
+    .then(data => {
+      const attachment = new discord.AttachmentBuilder(data, { name: "$username[$mentioned[1]].png"});
+      message.reply({ files: [attachment] });
+    })]`
+    
+}, {
+  name: "$alwaysExecute",
+  $if: "old",
+  code: ``}]
+    /*$author[👍| your levels;$userAvatar[$authorID]]
   $description[> Level: $getUserVar[lvl;$mentioned[1;true]]
   > xp: $getUserVar[xp;$mentioned[1;true]]
   > requirement: $getUserVar[xp;$mentioned[1;true]]/$getUserVar[reqxp;$mentioned[1;true]]
@@ -23,4 +57,4 @@ module.exports = [{
  $onlyif[$getUserVar[xp]>=$getUserVar[reqxp];]
  
  $onlyif[$isBot[$authorID]==false;]`
-}]
+}]*/
