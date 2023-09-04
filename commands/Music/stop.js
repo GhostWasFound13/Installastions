4432ab28ffec90462981d87b97714147559aae93
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-09-04 15:23:37 +0700

    Update stop.js

--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -6,7 +6,8 @@ module.exports = {
     $title[$getVar[stop]]
     $color[$getVar[color]]
     $stopTrack
-      $onlyIf[$voiceId!=;$getVar[errorjoin]]
+      $onlyIf[$checkCondition[$voiceID==$replaceText[$replaceText[$checkCondition[$voiceID[$clientID]==];true;$voiceID];false;$voiceID[$clientID]]]==true;$replaceText[$getVar[errorsameuser];{voice};<#$voiceID[$clientID]>]]
+$onlyIf[$voiceID!=;$getVar[errorjoin]]
        $onlyIf[$hasPlayer!=;false;$getVar[errorqueue]]
    
   
