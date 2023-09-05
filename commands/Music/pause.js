6251c84a80dd9b00f250a44b799dd592d638317b
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-09-05 17:08:42 +0700

    Update pause.js

--- commands/Music/pause.js
+++ commands/Music/pause.js
@@ -4,5 +4,6 @@ module.exports = {
     $title[The music has been successfully stopped temporarily✅]
     $color[$getVar[color]]
     $pauseTrack
+      $onlyif[$getglobaluservar[blacklist]==false;I have detected that you are on the bot's blacklist for the following reason reason: **$getglobaluservar[rblacklist]** If you think it was a mistake, contact an **administrator**.] 
     `
 }
