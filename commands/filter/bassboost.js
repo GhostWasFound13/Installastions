4cc28d3f1787661a552d1829274ace95aac047f6
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-09-06 18:44:17 +0700

    Filter [10/100]
    I

---
+++ commands/filter/bassboost.js
@@ -0,0 +1,14 @@
+module.exports = {
+ name: "bassboost",
+ code: `$let[s;$addFilter[{"bassBoost": "2.0"}]]
+ 
+ $title[Added Bassboost]
+ $description[Added **BassBoost** Filter Successfully!]
+ $footer[Added by $username]
+ $addTimestamp
+ $color[Random]
+ `
+
+
+  
+}
---
+++ commands/filter/nightcore.js
@@ -0,0 +1,14 @@
+module.exports = {
+ name: "nightcore",
+ code: `$let[s;$addFilter[{"nightCore": "1.2"}]]
+ 
+ $title[Added Nightcore]
+ $description[Added **Nigtcore** Filter Successfully!]
+ $footer[Added by $username]
+ $addTimestamp
+ $color[Random]
+ `
+
+
+  
+}
