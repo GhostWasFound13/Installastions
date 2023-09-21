396c45f203aeb57e23b96802f0c0e64c26437ad9
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-09-21 19:26:05 +0700

    Create RoundDown.js

---
+++ handler/functions/misc/RoundDown.js
@@ -0,0 +1,16 @@
+function roundDown(d) {
+  const data = d.util.aoiFunc(d);
+  const [number] = data.inside.splits;
+
+  if (isNaN(number)) {
+    return d.aoiError.fnError(d, 'custom', {}, 'Invalid input. Usage: $roundDown[number]');
+  }
+
+  const result = Math.floor(parseFloat(number));
+  data.result = result;
+
+  return {
+    code: d.util.setCode(data),
+  };
+}
+module.exports = roundDown;
