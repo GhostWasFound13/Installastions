80b11c94480f80bd8ed0fd8e3cae831e34ff34c8
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-09-21 14:43:34 +0700

    Update onReady.js

--- handler/loader/onReady.js
+++ handler/loader/onReady.js
@@ -1,8 +1,8 @@
 const config = require('../config.js')
 const { Events } = require('discord.js');
 
-const onReady = (bot) => {
-  bot.once(Events.ClientReady, c => {
+const onReady = (client) => {
+  client.once(Events.ClientReady, c => {
     setTimeout(async () => {
     const user = bot.user
     let r = [];
@@ -14,7 +14,7 @@ r.push(`         ${config.green}Ready${config.end}: ${config.skyBlue}true${confi
 r.push(`         ${config.green}Bot Creator${config.end}: ${config.red}${(await c.application.fetch()).owner.username + '#' + (await c.application.fetch()).owner.discriminator}${config.end}`)
 r.push(`         ${config.green}Guilds${config.end}: ${config.gray}${bot.guilds.cache.size}${config.end}`)
 r.push(`         ${config.green}Commands loaded${config.end}: ${config.gray}${bot.cmd.default.size}${config.end}`)
-r.push(`      ─━━━${config.whiteBg}Created by ahoemi${config.end}━━━━─`)
+r.push(`      ─━━━${config.whiteBg}Created by GhostWasFound13${config.end}━━━━─`)
 
 console.log(r.join('\n'))
     }, 2000)
