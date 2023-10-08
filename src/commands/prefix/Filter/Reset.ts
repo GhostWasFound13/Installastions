1522e88a43983d4fef85c585fecb7b6051dfdca8
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-10-08 14:04:28 +0700

    Update Reset.ts

--- src/commands/prefix/Filter/Reset.ts
+++ src/commands/prefix/Filter/Reset.ts
@@ -20,15 +20,32 @@ export default {
       `${client.i18n.get(language, "filters", "reset_loading")}`
     );
 
-    const player = client.manager.players.get(message.guild!.id);
+     const player = client.manager.players.get(message.guild!.id);
     if (!player)
-      return msg.edit(`${client.i18n.get(language, "noplayer", "no_player")}`);
+      return msg.edit({
+        embeds: [
+          new EmbedBuilder()
+            .setDescription(
+              `${client.i18n.get(language, "noplayer", "no_player")}`,
+            )
+            .setColor(client.color),
+        ],
+      });
     const { channel } = message.member!.voice;
     if (
       !channel ||
       message.member!.voice.channel !== message.guild!.members.me!.voice.channel
     )
-      return msg.edit(`${client.i18n.get(language, "noplayer", "no_voice")}`);
+       return msg.edit({
+        embeds: [
+          new EmbedBuilder()
+            .setDescription(
+              `${client.i18n.get(language, "noplayer", "no_voice")}`,
+            )
+            .setColor(client.color),
+        ],
+      }); 
+     
 
     const data = {
       op: "filters",
