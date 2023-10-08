a04565be0ce40aa51b3c63b595f41aec15c42bfc
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-10-08 14:01:20 +0700

    Update Speed.ts

--- src/commands/prefix/Filter/Speed.ts
+++ src/commands/prefix/Filter/Speed.ts
@@ -28,17 +28,30 @@ export default {
 
     const player = client.manager.players.get(message.guild!.id);
     if (!player)
-      return message.channel.send(
-        `${client.i18n.get(language, "noplayer", "no_player")}`
-      );
+      return msg.edit({
+        embeds: [
+          new EmbedBuilder()
+            .setDescription(
+              `${client.i18n.get(language, "noplayer", "no_player")}`,
+            )
+            .setColor(client.color),
+        ],
+      });
     const { channel } = message.member!.voice;
     if (
       !channel ||
       message.member!.voice.channel !== message.guild!.members.me!.voice.channel
     )
-      return message.channel.send(
-        `${client.i18n.get(language, "noplayer", "no_voice")}`
-      );
+       return msg.edit({
+        embeds: [
+          new EmbedBuilder()
+            .setDescription(
+              `${client.i18n.get(language, "noplayer", "no_voice")}`,
+            )
+            .setColor(client.color),
+        ],
+      }); 
+     
 
     if (Number(value) < 0)
       return message.channel.send(
