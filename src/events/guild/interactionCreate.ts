3fa6b7ff273fcb5caf1f400b41b984fdff76fd0d
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-10-09 15:44:28 +0700

    Update interactionCreate.ts

--- src/events/guild/interactionCreate.ts
+++ src/events/guild/interactionCreate.ts
@@ -18,16 +18,6 @@ import yts from "yt-search";
  * @param {GlobalInteraction} interaction
  */
 
-const REGEX = [
-  /(?:https?:\/\/)?(?:www\.)?youtu(?:\.be\/|be.com\/\S*(?:watch|embed)(?:(?:(?=\/[-a-zA-Z0-9_]{11,}(?!\S))\/)|(?:\S*v=|v\/)))([-a-zA-Z0-9_]{11,})/,
-  /^.*(youtu.be\/|list=)([^#\&\?]*).*/,
-  /^(?:spotify:|https:\/\/[a-z]+\.spotify\.com\/(track\/|user\/(.*)\/playlist\/|playlist\/))(.*)$/,
-  /^https?:\/\/(?:www\.)?deezer\.com\/[a-z]+\/(track|album|playlist)\/(\d+)$/,
-  /^(?:(https?):\/\/)?(?:(?:www|m)\.)?(soundcloud\.com|snd\.sc)\/(.*)$/,
-  /(?:https:\/\/music\.apple\.com\/)(?:.+)?(artist|album|music-video|playlist)\/([\w\-\.]+(\/)+[\w\-\.]+|[^&]+)\/([\w\-\.]+(\/)+[\w\-\.]+|[^&]+)/,
-  /^https?:\/\/(?:www\.|secure\.|sp\.)?nicovideo\.jp\/watch\/([a-z]{2}[0-9]+)/,
-];
-
 export default async (client: Manager, interaction: GlobalInteraction) => {
   if (
     interaction.isCommand() ||
@@ -92,69 +82,11 @@ export default async (client: Manager, interaction: GlobalInteraction) => {
       }
     });
 
-    // Push Function
-    async function AutoCompletePush(
-      url: string,
-      choice: AutocompleteInteractionChoices[]
-    ) {
-      const Random =
-        client.config.lavalink.DEFAULT[
-          Math.floor(Math.random() * client.config.lavalink.DEFAULT.length)
-        ];
-      const match = REGEX.some((match) => {
-        return match.test(url) == true;
-      });
-      if (match == true) {
-        choice.push({ name: url, value: url });
-        await (interaction as AutocompleteInteraction)
-          .respond(choice)
-          .catch(() => {});
-      } else {
-        if (client.lavalink_using.length == 0) {
-          choice.push({
-            name: `${client.i18n.get(language, "music", "no_node")}`,
-            value: `${client.i18n.get(language, "music", "no_node")}`,
-          });
-          return;
-        }
-        await client.manager.search(url || Random).then((result) => {
-          for (let i = 0; i < 10; i++) {
-            const x = result.tracks[i];
-            choice.push({ name: x.title, value: x.uri });
-          }
-        });
-        await (interaction as AutocompleteInteraction)
-          .respond(choice)
-          .catch(() => {});
-      }
-    }
+    if (!command) return;
 
     if (
       Number(interaction.type) == InteractionType.ApplicationCommandAutocomplete
-    ) {
-      if (
-        (interaction as CommandInteraction).commandName == "play" ||
-        (interaction as CommandInteraction).commandName + command.name[1] ==
-          "playlist" + "add"
-      ) {
-        let choice: AutocompleteInteractionChoices[] = [];
-        const url = (interaction as CommandInteraction).options.get(
-          "search"
-        )!.value;
-        return AutoCompletePush(url as string, choice);
-      } else if (
-        (interaction as CommandInteraction).commandName + command.name[1] ==
-        "playlist" + "edit"
-      ) {
-        let choice: AutocompleteInteractionChoices[] = [];
-        const url = (interaction as CommandInteraction).options.get(
-          "add"
-        )!.value;
-        return AutoCompletePush(url as string, choice);
-      }
-    }
-
-    if (!command) return;
+    ) return client.emit("autoComplete", interaction, language, command)
 
     const msg_cmd = [
       `[COMMAND] ${command.name[0]}`,
@@ -166,15 +98,9 @@ export default async (client: Manager, interaction: GlobalInteraction) => {
     client.logger.info(`${msg_cmd.join(" ")}`);
 
     if (command.owner && interaction.user.id != client.owner)
-      return (interaction as NoAutoInteraction).reply({
-        embeds: [
-          new EmbedBuilder()
-            .setDescription(
-              `${client.i18n.get(language, "interaction", "owner_only")}`,
-            )
-            .setColor(client.color),
-        ],
-      });
+      return (interaction as NoAutoInteraction).reply(
+        `${client.i18n.get(language, "interaction", "owner_only")}`
+      );
 
     try {
       if (command.premium) {
@@ -204,13 +130,7 @@ export default async (client: Manager, interaction: GlobalInteraction) => {
     } catch (err) {
       client.logger.error(err);
       return (interaction as NoAutoInteraction).reply({
-        embeds: [
-          new EmbedBuilder()
-            .setDescription(
-              `${client.i18n.get(language, "nopremium", "premium_error")}`,
-            )
-            .setColor(client.color),
-        ],
+        content: `${client.i18n.get(language, "nopremium", "premium_error")}`,
       });
     }
 
@@ -219,15 +139,15 @@ export default async (client: Manager, interaction: GlobalInteraction) => {
         PermissionsBitField.Flags.SendMessages
       )
     )
-          return interaction.user.dmChannel!.send({
-        embeds: [
-          new EmbedBuilder()
-            .setDescription(
-              `${client.i18n.get(language, "interaction", "no_perms")}`,
-            )
-            .setColor(client.color),
-        ],
-      });
+      return interaction.user.dmChannel!.send(
+        {
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(`${client.i18n.get(language, "interaction", "no_perms")}`)
+              .setColor(client.color)
+          ]
+        }
+      );
     if (
       !interaction.guild.members.me!.permissions.has(
         PermissionsBitField.Flags.ViewChannel
@@ -240,7 +160,13 @@ export default async (client: Manager, interaction: GlobalInteraction) => {
       )
     )
       return (interaction as NoAutoInteraction).reply(
-        `${client.i18n.get(language, "interaction", "no_perms")}`
+        {
+          embeds: [
+            new EmbedBuilder()
+              .setDescription(        `${client.i18n.get(language, "interaction", "no_perms")}`)
+              .setColor(client.color)
+          ]
+        }
       );
     if (!((interaction as CommandInteraction).commandName == "help")) {
       if (
@@ -248,63 +174,69 @@ export default async (client: Manager, interaction: GlobalInteraction) => {
           PermissionsBitField.Flags.Speak
         )
       )
-        return (interaction as NoAutoInteraction).reply({
-        embeds: [
-          new EmbedBuilder()
-            .setDescription(
-              `${client.i18n.get(language, "interaction", "no_perms")}`,
-            )
-            .setColor(client.color),
-        ],
-      });
+        return (interaction as NoAutoInteraction).reply(
+          {
+            embeds: [
+              new EmbedBuilder()
+                .setDescription(`${client.i18n.get(language, "interaction", "no_perms")}`)
+                .setColor(client.color)
+            ]
+          }
+        );
       if (
         !interaction.guild.members.me!.permissions.has(
           PermissionsBitField.Flags.Connect
         )
       )
-        return (interaction as NoAutoInteraction).reply({
-        embeds: [
-          new EmbedBuilder()
-            .setDescription(
-              `${client.i18n.get(language, "interaction", "no_perms")}`,
-            )
-            .setColor(client.color),
-        ],
-      });
+        return (interaction as NoAutoInteraction).reply(
+          {
+            embeds: [
+              new EmbedBuilder()
+                .setDescription(`${client.i18n.get(language, "interaction", "no_perms")}`)
+                .setColor(client.color)
+            ]
+          }
+        );
       if (
         !interaction.guild.members.me!.permissions.has(
           PermissionsBitField.Flags.ManageMessages
         )
       )
-        return (interaction as NoAutoInteraction).reply({
-        embeds: [
-          new EmbedBuilder()
-            .setDescription(
-              `${client.i18n.get(language, "interaction", "no_perms")}`,
-            )
-            .setColor(client.color),
-        ],
-      });
+        return (interaction as NoAutoInteraction).reply(
+          {
+            embeds: [
+              new EmbedBuilder()
+                .setDescription(`${client.i18n.get(language, "interaction", "no_perms")}`)
+                .setColor(client.color)
+            ]
+          }
+        );
       if (
         !interaction.guild.members.me!.permissions.has(
           PermissionsBitField.Flags.ManageChannels
         )
       )
-        return await (interaction as NoAutoInteraction).reply({
-        embeds: [
-          new EmbedBuilder()
-            .setDescription(
-              `${client.i18n.get(language, "interaction", "no_perms")}`,
-            )
-            .setColor(client.color),
-        ],
-      });
+        return await (interaction as NoAutoInteraction).reply(
+          {
+            embeds: [
+              new EmbedBuilder()
+                .setDescription(`${client.i18n.get(language, "interaction", "no_perms")}`)
+                .setColor(client.color)
+            ]
+          }
+        );
     }
 
     if (command.lavalink) {
       if (client.lavalink_using.length == 0)
         return (interaction as NoAutoInteraction).reply(
-          `${client.i18n.get(language, "music", "no_node")}`
+          {
+            embeds: [
+              new EmbedBuilder()
+                .setDescription(`${client.i18n.get(language, "music", "no_node")}`)
+                .setColor(client.color)
+            ]
+          }
         );
     }
 
