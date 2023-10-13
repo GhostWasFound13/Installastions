cb1f93649974086c368273a1739fbc1e1bdfceab
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2023-10-13 18:02:30 +0700

    Update AntiCrash.ts

--- src/handlers/Plugin/AntiCrash.ts
+++ src/handlers/Plugin/AntiCrash.ts
@@ -2,18 +2,10 @@ import { Manager } from "../manager.js";
 
 
 export default (client: manager) => {
-  process.on('unhandledRejection', (reason, promise) => {
-    client.logger.error('[ANTI-CRASH] unhandledRejection');
-    console.log(promise, reason);
-  });
-
-  process.on('uncaughtException', (err, origin) => {
-    client.logger.error('[ANTI-CRASH] uncaughtException');
-    console.log(err, origin);
-  });
-
-  process.on('uncaughtExceptionMonitor', (err, origin) => {
-    client.logger.error('[ANTI-CRASH] uncaughtExceptionMonitor');
-    console.log(err, origin);
-  });
+ process.on("unhandledRejection", (error) =>
+      client.logger.log({ level: "error", message: String(error) })
+    );
+    process.on("uncaughtException", (error) =>
+      client.logger.log({ level: "error", message: String(error) })
+    );
 };
